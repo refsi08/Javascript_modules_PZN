a3afeca7f87668865714de9e636b912d7f9a11f5
RefDev <62316890+RefDev-00@users.noreply.github.com>
2023-09-03 21:13:58 +0700

    first commit

---
+++ script/multiple.js
@@ -0,0 +1,13 @@
+// variable
+const name = "Hello Refsi";
+
+//function
+function sum(a, b) {
+  return a + b;
+}
+
+// class
+class Company {}
+
+// export multiple
+export { name, sum, Company };
---
+++ script/say.js
@@ -0,0 +1,11 @@
+//berikan export agar funtion bisa di expost keluar
+export function sayHello(name) {
+  console.log(`Hello ${name}`);
+}
+
+export function sayGoodBye(name) {
+  console.log(`Good Bye ${name}`);
+}
+
+// berikan export agar variable bisa di expost
+export const name = "Hello Javascript!";
